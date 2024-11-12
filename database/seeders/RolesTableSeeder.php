f1ec7862f8da32fdc9e8757c9ff2d66999a402ba
Mahran_AD <mhranabwdqt971@gmail.com>
2024-11-12 18:58:09 +0300

    initialize seeder for roles

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        {
+            $roles = [
+                [
+                    'id' => 1,
+                    'name'=> 'Admin',
+
+                ],
+                [
+                    'id' => 2,
+                    'name'=> 'Agent',
+
+                ]
+            ];
+            Role::insert($roles);
+        }
+    }
+}
