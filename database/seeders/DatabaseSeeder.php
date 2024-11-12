d303056212ee9250437d98b792f0c4cd3b4f5568
Mahran_AD <mhranabwdqt971@gmail.com>
2024-11-12 18:54:32 +0300

    some updates in User model and database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+       $this->call([
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            UsersTableSeeder::class,
+            RolesTableSeeder::class,
+            PermissionsTableSeeder::class,
+            RoleUserTableSeeder::class,
+            PermissionRoleTableSeeder::class,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    dd(User::find(1)->hasPermission('category_create'));
+//    return view('welcome');
 });
