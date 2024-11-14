bd54fb48100ccd8c923ae76ed2f3d4666c72afe2
Mahran_AD <mhranabwdqt971@gmail.com>
2024-11-15 01:06:22 +0300

    make edit password feature

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
-use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
@@ -13,7 +15,24 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Action::make('updatePassword')
+            ->form([
+                TextInput::make('password')
+                    ->required()
+                    ->password()
+                    ->confirmed('password'),
+
+                TextInput::make('password_confirmation')
+                    ->required()
+                    ->password()
+            ])->action(function(array $data) {
+                $this->record->update(['password'=>$data['password']]);
+
+                    Notification::make()
+                        ->title('Password updated successfully')
+                        ->success()
+                        ->send();
+            })
         ];
     }
 }
