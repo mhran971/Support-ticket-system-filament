a264d2a22b91ca9a54b9287183130b7f217bff94
Mahran_AD <mhranabwdqt971@gmail.com>
2024-11-15 15:11:29 +0300

    make max width of table full extra option sidebar Fully Collapsible On Desktop

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -28,6 +29,8 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->sidebarCollapsibleOnDesktop()
+            ->maxContentWidth(MaxWidth::Full)
+           // ->sidebarFullyCollapsibleOnDesktop()
             ->colors([
                 'primary' => Color::Purple,
             ])
