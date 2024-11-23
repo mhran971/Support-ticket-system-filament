148d2cc514e009b614073e6325b2e2927e29a5f8
Mahran_AD <mhranabwdqt971@gmail.com>
2024-11-24 01:29:54 +0300

    A guide to add Header Widgets

--- app/Filament/Resources/TicketResource/Pages/ListTickets.php
+++ app/Filament/Resources/TicketResource/Pages/ListTickets.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\TicketResource\Pages;
 
 use App\Filament\Resources\TicketResource;
+use App\Filament\Widgets\StatsOverview;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,12 @@ class ListTickets extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            StatsOverview::class
         ];
     }
 }
