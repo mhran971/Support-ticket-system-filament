4d2793a7ece8b6489a49ea9a0d4e261875bf2f93
Mahran_AD <mhranabwdqt971@gmail.com>
2024-11-28 02:13:49 +0300

    work on charts and Implement Filters Accordingly

---
+++ app/Filament/Widgets/TicketsOverviewChart.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Ticket;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class TicketsOverviewChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    public ?string $filter = 'week';
+    protected function getFilters(): ?array
+    {
+        return [
+            'week' => 'Last week',
+            'month' => 'Last month',
+            'year' => 'This year',
+        ];
+    }
+
+    protected function getData(): array
+    {
+        $start = null;
+        $end = null;
+        $perData = null;
+
+        switch ($this->filter){
+            case 'week';
+                $start =  now()->startOfWeek();
+                $end =  now()->endOfWeek();
+                $perData = 'perDay';
+                break;
+
+                case 'month';
+                $start = now()->startOfMonth();
+                $end = now()->endOfMonth();
+                $perData = 'perMonth';
+                break;
+
+            default:
+            case 'year';
+                $start = now()->startOfMonth();
+                $end = now()->endOfMonth();
+                $perData = 'perYear';
+                break;
+
+
+        }
+
+        $data = Trend::model(Ticket::class)
+            ->between(
+                start: $start,
+                end: $end,
+            )
+            ->$perData()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Ticket data',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
