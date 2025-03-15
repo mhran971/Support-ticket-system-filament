f080cf1bb95fb730d0bc5016163efec0c02bd347
Mahran_AD <mhranabwdqt971@gmail.com>
2025-03-15 16:03:19 +0300

    installing laravel reverb

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd(User::find(1)->hasPermission('category_create'));
-//    return view('welcome');
+//    dd(User::find(1)->hasPermission('category_create'));
+    return view('welcome');
 });
